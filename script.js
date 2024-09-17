040bcebe17cffd2adcd3373d0185fbba1211a379
kristupas <108592864+removeris@users.noreply.github.com>
2024-09-17 13:33:57 +0300

    Add operation buttons, update layout, update user input function

--- script.js
+++ script.js
@@ -1,58 +1,91 @@
-function enterNumber() {
-    const buttons = document.querySelectorAll(".number");
+function hasClass(elem, string) {
+    return Array.from(elem.classList).includes(string);
+}
+
+function isValidNumber(num) {
+    num = parseInt(num);
+    return number.length <= 16 || number < Number.MAX_SAFE_INTEGER;
+}
+
+function getUserInput() {
+    const buttons = document.querySelectorAll(".number, .operation");
 
     const result = document.querySelector("#result");
 
     number = result.textContent;
 
+    let operation;
+
     buttons.forEach((button) => {
         button.addEventListener("click", (e) => {
-            if(number.length < 17 || parseInt(number) <= Number.MAX_SAFE_INTEGER){
-                switch(e.target.id){
-                    case "one":
-                        number += '1';
-                        break;
-                    case "two":
-                        number += '2';
-                        break;
-                    case "three":
-                        number += '3';
-                        break;
-                    case "four":
-                        number += '4';
-                        break;
-                    case "five":
-                        number += '5';
-                        break;
-                    case "six":
-                        number += '6';
-                        break;
-                    case "seven":
-                        number += '7';
-                        break;
-                    case "eight":
-                        number += '8';
-                        break;
-                    case "nine":
-                        number += '9';
-                        break;
-                    case "zero":
-                        number += '0'
-                        break;
-                    case "return":
-                        number = number.slice(0, number.length - 1); 
-                        console.log(number.slice(0, number.length - 1));
-                        break;
-                    default:
-                        break;
+            if(hasClass(e.target, "number")){
+                if(isValidNumber(number)){
+                    switch(e.target.id){
+                        case "one":
+                            number += '1';
+                            break;
+                        case "two":
+                            number += '2';
+                            break;
+                        case "three":
+                            number += '3';
+                            break;
+                        case "four":
+                            number += '4';
+                            break;
+                        case "five":
+                            number += '5';
+                            break;
+                        case "six":
+                            number += '6';
+                            break;
+                        case "seven":
+                            number += '7';
+                            break;
+                        case "eight":
+                            number += '8';
+                            break;
+                        case "nine":
+                            number += '9';
+                            break;
+                        case "zero":
+                            number += '0';
+                            break;
+                        case "floating-point":
+                            number += ".";
+                            break;
+                        case "return":
+                            number = number.slice(0, number.length - 1); 
+                            break;
+                    }
+                }
+                else {
+                    alert("The number is too large.");
                 }
                 result.textContent = number;
             }
-            else {
-                alert("The number is too large.");
+            else if(hasClass(e.target, "operation")) {
+                switch(e.target.id){
+                    case "add":
+                        operation = "+";
+                        break;
+                    case "subtract":
+                        operation = "-";
+                        break;
+                    case "multiply":
+                        operation = "X";
+                        break;
+                    case "divide":
+                        operation = "/";
+                        break;
+                    case "equals":
+                        operation = "=";
+                        break;
+                }
+                console.log(operation);
             }
         })
     })
 }
 
-enterNumber();
+getUserInput();
